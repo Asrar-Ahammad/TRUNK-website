73c9c01c827a2ace863ba600da70a63f12fdd8e6
ShuaibAfnanSk <ivoxatomig@gmail.com>
2023-12-16 15:42:51 +0530

    made pages and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
-import NavBar from './components/NavBar/NavBar'
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Stay from './pages/Stay/Stay';
+import Home from './pages/Home/Home';
 
 function App() {
 
   return (
     <>
-      <NavBar/>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/stay' element={<Stay />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/Stay/Stay.jsx
@@ -0,0 +1,11 @@
+import "./Stay.css";
+
+const Stay = () => {
+    return ( 
+        <div className="stay">
+            <div className="stay-hero" />
+        </div>
+     );
+}
+ 
+export default Stay;
