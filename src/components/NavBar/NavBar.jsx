f54e31c7207e8dece8908063220a804f13dc0ca0
Asrar-Ahammad <asrarahammadshaik@gmail.com>
2023-12-16 15:46:31 +0530

    Second commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -21,41 +21,44 @@ const NavBar = () => {
         <div className="menu">
           <img src="src/assets/images/stay-book-now.svg" alt="" />
           <div className="menu-parent" onClick={handleToggle}>
-            <div className="menu-icon-border" ></div>
+            <div className="menu-icon-border"></div>
             <CiMenuFries className="menu-icon" />
           </div>
         </div>
       </nav>
-{openMenu && (<div className="nav-menu">
-        <div className="menu-parent" onClick={handleToggle}>
-          <div className="menu-icon-border" ></div>
-          <IoCloseOutline className="menu-icon" />
-        </div>
-        <div className="menu-list">
-          <ul>
-            {menu_list_items.map((item) => (
-              <li>{item.name}</li>
-            ))}
-          </ul>
-        </div>
+{openMenu && 
+  <div className="nav-menu">
+  <div className="menu-parent" onClick={handleToggle}>
+    <div className="menu-icon-border"></div>
+    <IoCloseOutline className="menu-icon"/>
+  </div>
+  <div className="menu-list">
+    <ul>
+      {menu_list_items.map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  </div>
 
-        <div className="menu-intro">
-          <h4>TRUNK（HOTEL）YOYOGI PARK</h4>
-          <p>1-15-2 Tomigaya Shibuya-ku Tokyo 151-0063 JAPAN</p>
-          <p>tel. 03-5454-3210</p>
-          <p>email. yoyogipark@trunk-hotel.com</p>
-          <FaInstagram className="menu-insta-icon" />
-          <h4>Our Hotels</h4>
-          <div className="privacy">
-            {terms.map((item) => (
-              <span>{item.name}</span>
-            ))}
-          </div>
-          <div className="copyrights">
-            <p>© TRUNK co.,ltd. All Rights Reserved.</p>
-          </div>
-        </div>
-      </div>)}
+  <div className="menu-intro">
+    <h4>TRUNK（HOTEL）YOYOGI PARK</h4>
+    <p>1-15-2 Tomigaya Shibuya-ku Tokyo 151-0063 JAPAN</p>
+    <p>tel. 03-5454-3210</p>
+    <p>email. yoyogipark@trunk-hotel.com</p>
+    <FaInstagram className="menu-insta-icon" />
+    <h4>Our Hotels</h4>
+    <div className="privacy">
+      {terms.map((item) => (
+        <span>{item.name}</span>
+      ))}
+    </div>
+    <div className="copyrights">
+      <p>© TRUNK co.,ltd. All Rights Reserved.</p>
+    </div>
+  </div>
+</div>
+  
+}
       
     </>
   );
